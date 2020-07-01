264115ea2ec3ed124ed65aff77fd3c99c53fcf4e
Scott O'Halloran <o_halloran@yahoo.com>
2020-07-01 06:59:48 -0700

    07/01/2020

---
+++ verb_lists/PullVerbsFromWeb.py
@@ -0,0 +1,193 @@
+'''
+Pulls a list of conjugated Irish verbs from http://www.teanglan.ie and writes to 
+a CSV file.
+'''
+import csv
+import requests
+import re
+from bs4 import BeautifulSoup
+
+input_file = open('verblist.csv', 'r')
+output_file = open('conjugated_verbs.csv','w')
+#delete contents of output file if it exists
+output_file.write('')
+output_file.close()
+output_file = open('conjugated_verbs.csv','a')
+
+
+error_file = open('error.txt','w')
+#delete contents of error file if it exists
+error_file.write('')
+error_file.close()
+error_file = open('error.txt','a')
+#Read headword and English from input CSV file
+csv_input = csv.reader(input_file, delimiter=',')
+for verb in csv_input:
+	headword = verb[0]
+	verb_english = verb[1] 
+	verb_type = verb[2]
+	verb_class = verb[3]
+	verb_conjugation = verb[4]
+	#Create string to hold verb conjugation and eventually write to output file
+	output_file_string = headword + "," + verb_english+ "," + verb_type + "," + verb_class+ "," + verb_conjugation + ","
+	URL = 'https://www.teanglann.ie/en/gram/' + headword
+	page = requests.get(URL)
+	soup = BeautifulSoup(page.content, 'html.parser')
+################################################################################
+# Verbal Noun
+################################################################################
+	for header in soup.find_all('h3', text=re.compile('VERBAL NOUN')):
+	    section = header.parent
+	    verbal_noun_resultset = section.find_all(class_='value primary')
+	    for verbal_noun in verbal_noun_resultset:
+	    	output_file_string += "," + verbal_noun.text
+################################################################################
+# Verbal Adjective
+################################################################################
+
+	for header in soup.find_all('h3', text=re.compile('VERBAL ADJECTIVE')):
+	    section = header.parent
+	    verbal_adjective_resultset = section.find_all(class_='value primary')
+	    for verbal_adjective in verbal_adjective_resultset:
+	    	output_file_string += "," + verbal_adjective.text
+################################################################################
+# Past Tense
+################################################################################
+
+	simple_past_array = []
+	simple_past = soup.find(id='past')
+	if simple_past == None:
+		error_file.write(headword + '\n')
+		#continue
+	else:
+		simple_past_elements = simple_past.find_all(class_='block introd')
+		for simple_past_element in simple_past_elements:
+			element_array = simple_past_element.text.replace('1', '').replace('2', '').replace('3', '').replace('▪', '').strip().split('\n')
+			for element in element_array:
+				simple_past_array.append(element)
+		
+		for word in simple_past_array:
+			output_file_string += ',' + word
+		output_file.write(output_file_string) 
+################################################################################
+# Present Tense
+################################################################################
+
+	present_array = []
+	present = soup.find(id='present')
+	if present == None:
+		continue
+	else:
+		present_elements = present.find_all(class_='block introd')
+		for present_element in present_elements:
+			element_array = present_element.text.replace('1', '').replace('2', '').replace('3', '').replace('▪', '').strip().split('\n')
+			for element in element_array:
+				present_array.append(element)
+		
+		for word in present_array:
+			output_file_string += ',' + word
+		output_file.write(output_file_string) 
+
+################################################################################
+# Future Tense
+################################################################################
+
+	future_array = []
+	future = soup.find(id='future')
+	if future == None:
+		continue
+	else:
+		future_elements = future.find_all(class_='block introd')
+		for future_element in future_elements:
+			element_array = future_element.text.replace('1', '').replace('2', '').replace('3', '').replace('▪', '').strip().split('\n')
+			for element in element_array:
+				future_array.append(element)
+		
+		for word in future_array:
+			output_file_string += ',' + word
+		output_file.write(output_file_string) 
+
+################################################################################
+# Conditional Mood
+################################################################################
+
+	conditional_array = []
+	conditional = soup.find(id='condi')
+	if conditional == None:
+		continue
+	else:
+		conditional_elements = conditional.find_all(class_='block introd')
+		for conditional_element in conditional_elements:
+			element_array = conditional_element.text.replace('1', '').replace('2', '').replace('3', '').replace('▪', '').strip().split('\n')
+			for element in element_array:
+				conditional_array.append(element)
+		
+		for word in conditional_array:
+			output_file_string += ',' + word
+		output_file.write(output_file_string)
+
+################################################################################
+# Past Continual Tense
+################################################################################
+
+	past_continual_array = []
+	past_continual = soup.find(id='pastConti')
+	if past_continual == None:
+		continue
+	else:
+		past_continual_elements = past_continual.find_all(class_='block introd')
+		for past_continual_element in past_continual_elements:
+			element_array = past_continual_element.text.replace('1', '').replace('2', '').replace('3', '').replace('▪', '').strip().split('\n')
+			for element in element_array:
+				past_continual_array.append(element)
+		
+		for word in past_continual_array:
+			output_file_string += ',' + word
+		output_file.write(output_file_string) 
+
+################################################################################
+# Imperative Mood
+################################################################################
+
+	imperative_array = []
+	imperative = soup.find(id='imper')
+	if imperative == None:
+		continue
+	else:
+		imperative_elements = imperative.find_all(class_='block introd')
+		for imperative_element in imperative_elements:
+			element_array = imperative_element.text.replace('1', '').replace('2', '').replace('3', '').replace('▪', '').strip().split('\n')
+			for element in element_array:
+				imperative_array.append(element)
+		
+		for word in imperative_array:
+			output_file_string += ',' + word
+		output_file.write(output_file_string) 
+
+################################################################################
+# Present Subjunctive Mood
+################################################################################
+
+	present_subjunctive_array = []
+	present_subjunctive = soup.find(id='subj')
+	if present_subjunctive == None:
+		continue
+	else:
+		present_subjunctive_elements = present_subjunctive.find_all(class_='block introd')
+		for present_subjunctive_element in present_subjunctive_elements:
+			element_array = present_subjunctive_element.text.replace('1', '').replace('2', '').replace('3', '').replace('▪', '').strip().split('\n')
+			for element in element_array:
+				present_subjunctive_array.append(element)
+		
+		for word in present_subjunctive_array:
+			output_file_string += ',' + word
+	output_file_string += "\n"
+	output_file.write(output_file_string) 
+
+
+
+
+output_file.close()
+
+
+
--- verb_lists/test.py
+++ verb_lists/test.py
@@ -1,49 +1,20 @@
-import requests
-from bs4 import BeautifulSoup
+################################################################################
+# Present Subjunctive Mood
+################################################################################
 
-URL = 'https://www.teanglann.ie/en/gram/achomharc'
-page = requests.get(URL)
-
-soup = BeautifulSoup(page.content, 'html.parser')
-
-past = soup.find(id='past')
-past_elements = past.find_all(class_='block introd')
-for past_element in past_elements:
-	element = past_element.text.replace('1', '').replace('2', '').replace('3', '').replace('▪', '').strip()
-	print(element)
-
-present = soup.find(id='present')
-present_elements = present.find_all(class_='block introd')
-for present_element in present_elements:
-	element = present_element.text.replace('1', '').replace('2', '').replace('3', '').replace('▪', '').strip()
-	print(element)
-
-future = soup.find(id='future')
-future_elements = future.find_all(class_='block introd')
-for future_element in future_elements:
-	element = future_element.text.replace('1', '').replace('2', '').replace('3', '').replace('▪', '').strip()
-	print(element)
-
-conditional = soup.find(id='condi')
-conditional_elements = conditional.find_all(class_='block introd')
-for conditional_element in conditional_elements:
-	element = conditional_element.text.replace('1', '').replace('2', '').replace('3', '').replace('▪', '').strip()
-	print(element)
-
-past_continual = soup.find(id='pastConti')
-past_continual_elements = past_continual.find_all(class_='block introd')
-for past_continual_element in past_continual_elements:
-	element = past_continual_element.text.replace('1', '').replace('2', '').replace('3', '').replace('▪', '').strip()
-	print(element)
-
-imperative = soup.find(id='imper')
-imperative_elements = imperative.find_all(class_='block introd')
-for imperative_element in imperative_elements:
-	element = imperative_element.text.replace('1', '').replace('2', '').replace('3', '').replace('▪', '').strip()
-	print(element)
-
-present_subjunctive = soup.find(id='subj')
-present_subjunctive_elements = present_subjunctive.find_all(class_='block introd')
-for present_subjunctive_element in present_subjunctive_elements:
-	element = present_subjunctive_element.text.replace('1', '').replace('2', '').replace('3', '').replace('▪', '').strip()
-	print(element)
+	present_subjunctive_array = []
+	past = soup.find(id='present_subjunctive')
+	if present_subjunctive == None:
+		error_file.write(headword + '\n')
+		#continue
+	else:
+		present_subjunctive_elements = present_subjunctive.find_all(class_='block introd')
+		for present_subjunctive_element in present_subjunctive_elements:
+			element_array = present_subjunctive_element.text.replace('1', '').replace('2', '').replace('3', '').replace('▪', '').strip().split('\n')
+			for element in element_array:
+				present_subjunctive_array.append(element)
+		
+		for word in present_subjunctive_array:
+			output_file_string += ',' + word
+		output_file_string += "\n"
+		output_file.write(output_file_string) 
